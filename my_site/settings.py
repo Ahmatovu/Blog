4c9519592b5b9c7c73a17e08b3921096297a82c3
Ahmed <103221893+Ahmatovu@users.noreply.github.com>
2023-08-07 15:23:29 +0300

    Update settings.py
    allowed hosts

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'SK'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1:8000','AhmedSajjid85.pythonanywhere.com']
 
 
 # Application definition
